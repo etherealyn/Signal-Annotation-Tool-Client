525c799b761da657fa5ceee0e61feaa621ab0887
Aziret Satybaldiev <satybaldiev.aziret@gmail.com>
2019-02-03 14:31:06 +0100

    Slightly improve UX for logins screen

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import {UserAuthorization} from '../../models/user.authorization.model';
-import {AuthService} from '../auth.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {Subscription} from 'rxjs';
-import {HttpErrorResponse} from '@angular/common/http';
-import {catchError} from 'rxjs/operators';
+import { UserAuthorization } from '../../models/user.authorization.model';
+import { AuthService } from '../auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ClrLoadingState } from '@clr/angular';
 
 @Component({
   selector: 'app-auth',
@@ -14,12 +13,16 @@ import {catchError} from 'rxjs/operators';
 })
 export class LoginComponent implements OnInit, OnDestroy {
   authModel = new UserAuthorization('', '', true);
+
   returnUrl: string;
   loading = false;
   error = false;
 
-  private subscription: Subscription;
   errorMessage: string;
+
+  loginBtnState: ClrLoadingState = ClrLoadingState.DEFAULT;
+
+  private _subscription: Subscription;
 
   constructor(private authService: AuthService,
               private route: ActivatedRoute,
@@ -38,24 +41,28 @@ export class LoginComponent implements OnInit, OnDestroy {
     // todo: add form validation
 
     this.loading = true;
+    this.loginBtnState = ClrLoadingState.LOADING;
     this.error = false;
 
-    this.subscription = this.authService.login(this.authModel.username, this.authModel.password)
+    this._subscription = this.authService.login(this.authModel.username, this.authModel.password)
       .subscribe(
         data => {
           if (data) {
             this.loading = false;
+            this.loginBtnState = ClrLoadingState.SUCCESS;
             this.router.navigate([this.returnUrl]);
           } else {
             this.loading = false;
+            this.loginBtnState = ClrLoadingState.ERROR;
             this.error = true;
           }
         }, error => {
           this.loading = false;
           this.error = true;
+          this.loginBtnState = ClrLoadingState.ERROR;
 
           if (error === 'Unknown Error') {
-            this.errorMessage = 'Oops! Something bad happened! :(';
+            this.errorMessage = 'Unknown Error. Check your internet connection.';
           } else if (error === 'Forbidden') {
             this.errorMessage = 'Such username and password combination does not match any our records.';
           }
@@ -63,8 +70,8 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
+    if (this._subscription) {
+      this._subscription.unsubscribe();
     }
   }
 }
