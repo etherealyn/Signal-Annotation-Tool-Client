c231a9fa42ae75b74aa19cd2bd36bac30ede2138
Aziret Satybaldiev <satybaldiev.aziret@gmail.com>
2019-02-04 14:32:09 +0100

    improved logic

--- src/app/editor/annotation/annotation.component.ts
+++ src/app/editor/annotation/annotation.component.ts
@@ -6,9 +6,13 @@ class Range {
   startTime: number;
   endTime: number;
 
-  constructor(startTime: number, endTime?: number) {
+  constructor(startTime: number, endTime: number) {
     this.startTime = startTime;
-    this.endTime = null;
+    this.endTime = 0;
+  }
+
+  public toString = (): string => {
+    return `Range (${this.startTime}, ${this.endTime})`;
   }
 }
 
@@ -54,74 +58,43 @@ export class AnnotationComponent implements OnInit {
   ];
 
   private _timeUpdateSubscription: Subscription;
+  debug = JSON.stringify(this.classes);
 
   ngOnInit() {
   }
 
-  onStartRecord(api: VgAPI, timeout = 1000) {
-    this.timerHandle = setInterval(() => {
-
-      this.classes.forEach(((x, i, xs) => {
-        if (x.checked) {
-          console.log(x.name, api.currentTime);
-
-          /** if the array is empty*/
-          if (x.series.length === 0) {
-            x.series.push({
-              startTime: api.currentTime,
-              endTime: null
-            });
-          } else {
-            const range = x.series[x.series.length - 1];
-
-            /** if the endTime is null, then the previous labelling did not finish*/
-            if (range.endTime !== null) {
-              x.series.push({
-                startTime: api.currentTime,
-                endTime: null
-              });
-            }
-          }
-        } else {
-          if (x.series.length === 0) {
-
-          } else {
-            const range = x.series[x.series.length - 1];
-            range.endTime = api.currentTime;
-          }
-        }
-        console.log(x.name, x.series);
-      }));
-
-    }, timeout);
-  }
-
-  onStopRecord() {
-    clearInterval(this.timerHandle);
-  }
-
   startRecording(vgAPI: VgAPI) {
-    this._timeUpdateSubscription = vgAPI.subscriptions.timeUpdate.subscribe((() => {
-      this.classes.forEach((classification => {
-        const name = classification.name;
-        const series = classification.series;
+    if (!this._timeUpdateSubscription) {
+      this._timeUpdateSubscription = vgAPI.subscriptions.timeUpdate.subscribe((() => {
+        this.classes.forEach((classification => {
+          const name = classification.name;
+          const series = classification.series;
 
-        if (classification.checked) {
-          if (series.length === 0 || (series.length !== 0 && series[series.length - 1].endTime !== null)) {
-            console.log(name, `labelling started at ${vgAPI.currentTime}`);
-            classification.isBeingLabelled = true;
-            series.push(new Range(vgAPI.currentTime));
-          }
-        } else {
-          if (classification.isBeingLabelled) {
-            console.log(name, `labelling finished at ${vgAPI.currentTime}`);
-            classification.isBeingLabelled = false;
-            if (series.length !== 0) {
-              series[series.length - 1].endTime = vgAPI.currentTime;
+          if (classification.checked) {
+            if (series.length === 0 || (series.length !== 0 && series[series.length - 1].endTime !== 0)) {
+              console.log(name, `labelling started at ${vgAPI.currentTime}`);
+              classification.isBeingLabelled = true;
+              series.push(new Range(vgAPI.currentTime, 0));
+            }
+          } else {
+            if (classification.isBeingLabelled) {
+              console.log(name, `labelling finished at ${vgAPI.currentTime}`);
+              classification.isBeingLabelled = false;
+              if (series.length !== 0) {
+                const range = series[series.length - 1];
+                range.endTime = vgAPI.currentTime;
+                series[series.length - 1] = range;
+              }
+              console.log(name, series);
             }
           }
-        }
+
+        }));
       }));
-    }));
+    }
+  }
+
+  get diagnostic() {
+    return JSON.stringify(this.classes);
   }
 }
