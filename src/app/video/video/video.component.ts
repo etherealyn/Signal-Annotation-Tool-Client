7f38f630faf704edcc59e3ad8abf56f8397daf4e
Aziret Satybaldiev <satybaldiev.aziret@gmail.com>
2019-03-06 12:53:43 +0100

    Initialize values of currentTime and durations correctly

--- src/app/video/video/video.component.ts
+++ src/app/video/video/video.component.ts
@@ -1,23 +1,20 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output } from '@angular/core';
 import { VgAPI } from 'videogular2/core';
 import { Subscription } from 'rxjs';
 import { ProjectsService } from '../../projects/projects.service';
-import { IMediaSubscriptions } from 'videogular2/src/core/vg-media/i-playable';
 
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: [ './video.component.scss' ]
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent implements OnDestroy {
   @Input() source: String;
   @Output() playerReady: EventEmitter<any> = new EventEmitter();
 
   api: VgAPI;
   url: String;
 
-  // currentTime = 0;
-  // duration = 0;
 
   private subscription: Subscription;
 
@@ -25,21 +22,14 @@ export class VideoComponent implements OnInit {
     this.url = `${projectsService.projectsUrl}/files`;
   }
 
-  ngOnInit() {
-  }
-
   onPlayerReady$(api: VgAPI) {
     this.api = api;
     this.playerReady.emit(api);
+  }
 
-    // const subscriptions: IMediaSubscriptions = api.subscriptions;
-
-    // this.subscription = subscriptions.timeUpdate.subscribe(() => {
-    //   this.currentTime = this.api.currentTime;
-    // });
-    //
-    // this.subscription = subscriptions.durationChange.subscribe(() => {
-    //   this.duration = api.duration;
-    // });
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
--- src/app/video/videogrid/videogrid.component.ts
+++ src/app/video/videogrid/videogrid.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { VgAPI } from 'videogular2/core';
 import { IVideo } from '../video.interface';
 import { ProjectEditorService } from '../../editor/project-editor.service';
@@ -6,36 +6,28 @@ import { LinkedList } from 'typescript-collections';
 import { IMediaSubscriptions } from 'videogular2/src/core/vg-media/i-playable';
 import { VideoService } from '../video.service';
 import { Hotkey, HotkeysService } from 'angular2-hotkeys';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-videogrid',
   templateUrl: './videogrid.component.html',
   styleUrls: [ './videogrid.component.scss' ]
 })
-export class VideogridComponent implements OnInit {
-  currentTime = 0;
-
+export class VideogridComponent implements OnInit, OnDestroy {
   private _videoSources: IVideo[] = [];
   private apis: LinkedList<VgAPI> = new LinkedList<VgAPI>();
-
-  private cursorSound = true;
-
+  private currentTimes: number[] = [];
+  private durations: number[] = [];
+  private cursorSound = false;
   private guard = 0;
   private isPlaying = false;
-
-  private progressDelayMilli = 50;
   private lastMouseLeft = 0;
-
   private playbackIndex = 3;
   private playbackValues: string[] = [ '0.25', '0.5', '0.75', '1.0', '1.25', '1.50', '1.75', '2.0', '3.0' ];
+  private subscription: Subscription;
+  private mainIndex = 0; // 'main' video index
 
-  private subscription;
-  private timeUpdateSubscription;
-
-  constructor(private videoService: VideoService,
-              private editorService: ProjectEditorService,
-              private hotkeysService: HotkeysService
-  ) {
+  constructor(private videoService: VideoService, private editorService: ProjectEditorService, private hotkeysService: HotkeysService) {
     this.registerHotkeys();
   }
 
@@ -58,27 +50,46 @@ export class VideogridComponent implements OnInit {
     }, undefined, 'Enable/disable `Cursor Sound` mode: an audio source is selected on mouse hover (default On)');
 
     const back = new Hotkey('left', (): boolean => {
-      this.seekTime(this.currentTime - 5);
+      this.seekTime(this.getCurrentTime() - 5);
       return false;
     }, undefined, '5 seconds back');
 
     const windback = new Hotkey('shift+left', (): boolean => {
-      this.seekTime(this.currentTime - 15);
+      this.seekTime(this.getCurrentTime() - 15);
       return false;
     }, undefined, '15 seconds back');
 
     const forward = new Hotkey('right', (): boolean => {
-      this.seekTime(this.currentTime + 5);
+      this.seekTime(this.getCurrentTime() + 5);
       return false;
     }, undefined, '5 seconds forward');
 
     const windforward = new Hotkey('shift+right', (): boolean => {
-      this.seekTime(this.currentTime + 15);
+      this.seekTime(this.getCurrentTime() + 15);
       return false;
     }, undefined, '15 seconds forward');
 
     [ playVideoHotkey, nextPlayback, prevPlayback, cursorSound, back, windback, forward, windforward ]
       .forEach(hotkey => this.hotkeysService.add(hotkey));
+  }
+
+  private onPlayerReady(api: VgAPI, index: number) {
+    this.apis.add(api);
+    this.videoService.onPlayerReady(api);
+
+    const subscriptions: IMediaSubscriptions = api.subscriptions;
+
+    this.subscription.add(subscriptions.canPlay.subscribe(() => {
+      this.currentTimes[index] = api.currentTime;
+    }));
+
+    this.subscription.add(subscriptions.timeUpdate.subscribe(() => {
+      this.currentTimes[index] = api.currentTime;
+    }));
+
+    this.subscription.add(subscriptions.durationChange.subscribe(() => {
+      this.durations[index] = api.duration;
+    }));
   }
 
   ngOnInit() {
@@ -93,26 +104,13 @@ export class VideogridComponent implements OnInit {
         }));
 
         this._videoSources = videos;
+        this.durations = new Array<number>(videos.length);
+        this.currentTimes = new Array<number>(videos.length);
       }
     });
   }
 
-  private onPlayerReady(api: VgAPI) {
-    this.apis.add(api);
-    this.videoService.onPlayerReady(api);
-
-    if (this.timeUpdateSubscription) {
-
-    } else {
-      const mediaSubscriptions: IMediaSubscriptions = this.apis.first().subscriptions;
-      this.timeUpdateSubscription = mediaSubscriptions.timeUpdate.subscribe(() => {
-        this.currentTime = this.apis.first().currentTime;
-      });
-    }
-  }
-
   /** Mouse Event Reactions  START */
-
   onMouseEnter(i: number) {
     if (this.cursorSound) {
       this.apis.elementAtIndex(i).volume = 1;
@@ -174,7 +172,6 @@ export class VideogridComponent implements OnInit {
     this.isPlaying = false;
   }
 
-
   nextPlaybackSpeed() {
     this.playbackIndex = (this.playbackIndex + 1) % this.playbackValues.length;
     this.apis.forEach((api: VgAPI) => {
@@ -196,32 +193,25 @@ export class VideogridComponent implements OnInit {
     return this.playbackValues[this.playbackIndex];
   }
 
-  @HostListener('window:keydown', [ '$event' ])
-  onKeyDown(event: KeyboardEvent) {
-    // console.log(event.code, event.shiftKey);
-    // if (event.code === 'Space') {
-    //   this.onPlayPause();
-    // }
-    //
-    // if (event.shiftKey) {
-    //   if (event.code === 'Period') {
-    //     this.nextPlaybackSpeed();
-    //   } else if (event.code === 'Comma') {
-    //     this.previousPlaybackSpeed();
-    //   }
-    // }
-  }
-
   /** Video Controls END*/
 
   get videoSources(): IVideo[] {
     return this._videoSources;
   }
 
-  get duration(): number {
-    const first = this.apis.first();
-    if (first) {
-      return first.duration;
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
+  }
+
+  getDuration() {
+    const duration: number = this.durations[this.mainIndex];
+    return duration ? Number.isNaN(duration) ? 0 : duration : 0;
+  }
+
+  getCurrentTime() {
+    const currentTime: number = this.currentTimes[this.mainIndex];
+    return currentTime ? Number.isNaN(currentTime) ? 0 : currentTime : 0;
   }
 }
